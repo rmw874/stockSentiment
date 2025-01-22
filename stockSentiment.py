8482a6559e1bf0af89c0a92d1b1732ad4fcf5a1e
Oscar Heiberg <rmw874@alumni.ku.dk>
2025-01-23 00:14:07 +0100

    dsstore

--- stockSentiment.py
+++ stockSentiment.py
@@ -3,8 +3,7 @@ import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-def FMPNews(page=0, size=5):
+def FMPNews(page=0, num_articles=1):
     key = config.get('FMP','API_KEY')
     return f'https://financialmodelingprep.com/api/v3/fmp/articles?page={page}&size={size}&apikey={key}'
 
-print(FMPNews())
